4be68c5ea9cfa9f298fd235fc8e4958549080238
euheber <euheber1@gmail.com>
2023-03-31 19:14:58 -0300

    fix: import and never using function

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { information } from "./components/information";
-import { plans } from "./components/plans";
-import { addons } from "./components/addons";
-import { sumamary } from "./components/summary";
-
 
 
 const formSection = document.querySelector("#formSection") as HTMLElement
 
-const reload = () => {
-  formSection.appendChild(information("olá, mundo"))
+const reload = ():void => {
+  formSection.innerHTML = information()
 }   
 
+
 reload()
+
