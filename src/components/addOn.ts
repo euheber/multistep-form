798b11166245d0a383c8d4da28adc208565e65fc
euheber <euheber1@gmail.com>
2023-04-04 18:17:43 -0300

    feat: form components

---
+++ src/components/addOn.ts
@@ -0,0 +1,3 @@
+export function addOns():string{
+    return 'addons'
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { information } from "./components/information";
-
-
+import { plan } from "./components/choosePlan";
+import { addOns } from "./components/addOn";
+import { summary } from "./components/summary";
 const formSection = document.querySelector("#formSection") as HTMLElement
 
-const reload = ():void => {
-  formSection.innerHTML = information()
+
+const htmlList: Array<string> = [information(), plan(), addOns(), summary()]
+
+const controlFormOfDisplay = ():void => {
+  formSection.innerHTML = htmlList[0]
 }   
 
-
-reload()
+controlFormOfDisplay()
 
