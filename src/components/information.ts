29589b235e11d988be6f84579b38ed72b70fc864
euheber <euheber1@gmail.com>
2023-04-05 13:18:16 -0300

    feat: controling components logic

--- src/components/information.ts
+++ src/components/information.ts
@@ -1,21 +1,23 @@
 export function information(): string {
   return `
-  <h2 class="text-4xl text-Marine-blue font-semibold">Informações Pessoais</h2>
-  <span class="text-Cool-gray mb-8 block">Forneça seu nome, email e número de telefone</span>
-    <form id="form" class="form space-y-5">
+    <form id="form" class="form space-y-4">
+  <div>
+      <h2 class="text-4xl text-Marine-blue font-semibold">Informações Pessoais</h2>
+      <span class="text-Cool-gray mb-5 block">Forneça seu nome, email e número de telefone</span>
+  </div>
         <div class="form-field flex flex-col">
           <label for="name" class="text-Purplish-blue font-semibold">Nome</label>
-          <input type="text" placeholder="Digite seu some" id="name" class="border border-Cool-gray p-2 rounded-lg">
+          <input type="text" placeholder="Digite seu some" id="name" class="border border-Cool-gray p-2 rounded-lg inputField">
         </div>
 
         <div class="form-field flex flex-col">
           <label for="email" class="text-Purplish-blue font-semibold">Email</label>
-          <input type="text" id="email" placeholder="Digite seu email" class="border border-Cool-gray p-2 rounded-lg">
+          <input type="text" id="email" placeholder="Digite seu email" class="border border-Cool-gray p-2 rounded-lg inputField">
         </div>
 
         <div class="form-field flex flex-col">
           <label for="phone" class="text-Purplish-blue font-semibold">Telefone</label>
-          <input type="text" id="number" placeholder="Digite seu número de telefone" class="border border-Cool-gray p-2 rounded-lg">
+          <input type="text" id="number" placeholder="Digite seu número de telefone" class="border border-Cool-gray p-2 rounded-lg inputField">
         </div>
   </form>
 
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,47 @@
-import { information } from "./components/information";
-import { plan } from "./components/choosePlan";
-import { addOns } from "./components/addOn";
-import { summary } from "./components/summary";
-const formSection = document.querySelector("#formSection") as HTMLElement
+import { information } from "./components/information"
+import { plan } from "./components/choosePlan"
+import { addOns } from "./components/addOn"
+import { summary } from "./components/summary"
 
+const formSection = document.querySelector("#form") as HTMLElement
+const nextBtn = document.querySelector("#nextBtn") as HTMLButtonElement
+const returnBtn = document.querySelector('#returnBtn') as HTMLButtonElement
+const btns = document.querySelectorAll('.btn') as NodeListOf<HTMLButtonElement>
 
 const htmlList: Array<string> = [information(), plan(), addOns(), summary()]
 
-const controlFormOfDisplay = ():void => {
-  formSection.innerHTML = htmlList[0]
-}   
+let activeForm = 0
+let activeBtn:HTMLButtonElement;
+const controlFormOfDisplay = (): void => {
+  formSection.innerHTML = htmlList[activeForm]
+  currentStep()
+}
+
+const currentStep = ():void => {
+  if(activeForm > 3 || activeForm < 0) return
+  if(activeBtn) activeBtn.classList.remove('bg-Pastel-blue')
+  btns.forEach((btn, index) => { 
+    if(activeForm == index) { 
+      activeBtn = btn
+      btn.classList.add('bg-Pastel-blue')
+    }
+   
+  })
+}
+
+
+nextBtn.addEventListener("click", function () {
+  currentStep()
+  if (activeForm >= 3) return
+  activeForm++
+  controlFormOfDisplay()
+})
+
+returnBtn.addEventListener('click', () => { 
+  currentStep()
+  if(activeForm <= 0) return
+  activeForm--
+  controlFormOfDisplay()
+})
 
 controlFormOfDisplay()
-
