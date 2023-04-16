2383222c35bf346f809caa0787dff10530016be5
euheber <euheber1@gmail.com>
2023-04-16 19:35:26 -0300

    feat: active plan

--- src/components/choosePlan.ts
+++ src/components/choosePlan.ts
@@ -2,8 +2,9 @@ import { plans } from "./plans"
 
 export function plan(): HTMLFormElement {
   const form = document.createElement("form")
+  form.classList.add('flex', 'gap-2')
 
-  form.append(plans('Arcade', 9), plans('Pro', 20))
+  form.append(plans('Arcade', 9), plans('Advanced', 20), plans('Pro', 30))
 
   return form
 }
--- src/components/plans.ts
+++ src/components/plans.ts
@@ -1,24 +1,39 @@
-export function plans(plan:string, price: number):HTMLDivElement{
-    const box = document.createElement('div')
-    box.classList.add('border', 'border-Cool-gray', 'rounded-lg', 'w-32', 'h-32',  'p-2', 'hover:border-Purplish-blue', 'transition', 'duration-150', 'cursor-pointer')
+let activeBox: HTMLDivElement
+export function plans(plan: string, price: number): HTMLDivElement {
+  const box = document.createElement("div")
+  box.classList.add(
+    "border",
+    "plan",
+    "border-Cool-gray",
+    "rounded-lg",
+    "w-32",
+    "h-32",
+    "p-2",
+    "hover:border-Purplish-blue",
+    "transition",
+    "duration-150",
+    "cursor-pointer"
+  )
 
-    box.addEventListener('click', ()=> { console.log('olá');
-    })
- 
-    const img = document.createElement('img')
-    img.src =  '/assets/images/icon-arcade.svg'
-    img.alt = 'Icone arcade'
-    img.classList.add('mb-5')
+  const img = document.createElement("img")
+  img.src = `/assets/images/icon-${plan.toLowerCase()}.svg`
+  img.alt = "Icone arcade"
+  img.classList.add("mb-5")
 
-    const h1 = document.createElement('h1')
-    h1.textContent =  plan
-    h1.classList.add('text-Marine-blue', 'font-semibold')
+  const h1 = document.createElement("h1")
+  h1.textContent = plan
+  h1.classList.add("text-Marine-blue", "font-semibold")
 
-    const span = document.createElement('span')
-    span.textContent =  `R$${price},00/mes`
-    span.classList.add('text-Cool-gray', 'text-sm')
+  const span = document.createElement("span")
+  span.textContent = `R$${price},00/mes`
+  span.classList.add("text-Cool-gray", "text-sm")
 
-    box.append(img, h1, span)
-    return box
+  box.append(img, h1, span)
+  box.addEventListener("click", () => {
+    const boxes = document.querySelectorAll(".plan") as NodeListOf<HTMLDivElement>
+    boxes.forEach((box) => box.classList.remove("border-Purplish-blue"))
+
+    box.classList.add("border-Purplish-blue")
+  })
+  return box
 }
-
